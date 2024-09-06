c9c6d66ae92376b63f2ef14fc1e448896bd1f777
fabian-guevara <fabiangviveros@gmail.com>
2024-09-06 15:28:43 -0600

    added .env variables

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,10 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';    
 
+dotenv.config(); // Load environment variables from .env file
 
 // MongoDB connection URI
-const uri = "CONNECTION"
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 await client.connect(); // Ensure the client is connected
