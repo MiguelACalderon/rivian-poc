830165261978c5f9fac96c5bfbbff084dad1cb62
fabian-guevara <fabiangviveros@gmail.com>
2024-09-13 10:27:45 -0600

    writting to s3 from pipeline

--- dataFederation.js
+++ dataFederation.js
@@ -1,7 +1,25 @@
 import client from "./dataFederationMongoDB.js";
 
-const query =  await client.db("rivian")
-    .collection('notification')
-    .find({ title: "Hello, World!"}).toArray();
+const query =  await client.db("rivian").collection('notification').find({ title: "Hello, World!"}).toArray();
+
+
+
+
+const outStage = {
+    $out: {
+        s3: {
+            bucket: 'fabian-rsc',
+            filename: "hello_rivian.json"
+        }
+    }
+};
+
+ 
+
+const out = await client.db("rivian").collection('notification').aggregate([outStage]);
+// AFTER PUSHING FILES TO S3, WE CAN NOW INTEGRATE DATABRICKS
+
+out.tryNext();
 
 console.log(query);
+
